af3dcda75b19b84d4f54db7dba76088b57146983
hanshal101 <82961842+hanshal101@users.noreply.github.com>
2023-01-21 20:43:21 +0530

    Create COUNTING-WORDS.cpp

---
+++ COUNTING-WORDS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,m;
+	    cin>>n>>m;
+	    
+	    cout<<m*n<<endl;
+	}
+	return 0;
+}
