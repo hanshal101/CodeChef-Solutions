26e48b361c657ce1fe0c499a459ee2fc44a1a451
hanshal101 <82961842+hanshal101@users.noreply.github.com>
2023-01-21 05:53:01 +0530

    Create DETSCORE.cpp

---
+++ DETSCORE.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    
+	    int x , n;
+	    cin>>x>>n;
+	    
+	    int total = x * n/10;
+	    cout<<total<<endl;
+	    
+	}
+	return 0;
+}
