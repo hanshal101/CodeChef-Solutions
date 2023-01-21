fea15fb28bc0b812fb427b6c39956de0cda649a1
hanshal101 <82961842+hanshal101@users.noreply.github.com>
2023-01-21 20:53:50 +0530

    Create INSTAGRAM.cpp

---
+++ INSTAGRAM.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a;
+	    cin>>a;
+	    int b;
+	    cin>>b;
+	    
+	    int c = b * 10;
+	    
+	    
+	    
+	    if(c<a){
+	        cout<<"YES"<<endl;
+	        
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
