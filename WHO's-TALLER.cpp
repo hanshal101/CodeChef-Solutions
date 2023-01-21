0a3a7090288f0acc921c60b9c67512b011c2ef52
hanshal101 <82961842+hanshal101@users.noreply.github.com>
2023-01-21 20:38:17 +0530

    Create WHO's-TALLER.cpp

---
+++ WHO's-TALLER.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x , y;
+	    cin>>x>>y;
+	    
+	    if (y<x){
+	        cout<<"A"<<endl;
+	        
+	        
+	    }
+	    
+	    else{
+	        cout<<"B"<<endl;
+	    }
+	}
+	return 0;
+}
