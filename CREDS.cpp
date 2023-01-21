2e730611a28ee4d1858d30e856bd89ddc9afc9fd
hanshal101 <82961842+hanshal101@users.noreply.github.com>
2023-01-21 20:49:19 +0530

    Create CREDS.cpp

---
+++ CREDS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x , y , z;
+	    cin>>x>>y>>z;
+	    int total = 4 * x + 2 * y ;
+	    cout<<total<<endl;
+	}
+	return 0;
+}
