0c7b5e450b7b2cfa8603620c171f749ad14f851f
hanshal101 <82961842+hanshal101@users.noreply.github.com>
2023-01-21 05:49:49 +0530

    Create WATERCONS.cpp

---
+++ WATERCONS.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,a;
+	cin>>t;
+	while(t--){
+	    cin>>a;
+	    if(2000 <= a){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
