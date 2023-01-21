1fe191e159fdf3b3a66ce3c2fa2baa6ae1afb0e8
hanshal101 <82961842+hanshal101@users.noreply.github.com>
2023-01-21 05:48:56 +0530

    Create SQUATS.cpp

---
+++ SQUATS.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,a;
+	cin>>t;
+	while(t--){
+	    cin>>a;
+	   int total = a*15;
+	    cout<<total<<endl;
+	}
+	return 0;
+}
