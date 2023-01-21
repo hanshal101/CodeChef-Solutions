909b29c5669ade04a2071b874f5aa3a319b6c4ee
hanshal101 <82961842+hanshal101@users.noreply.github.com>
2023-01-21 20:53:06 +0530

    Create DISCNT.cpp

---
+++ DISCNT.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    int pay = 100 - x;
+	    cout<<pay<<endl;
+	}
+	return 0;
+}
