94c3503a556cd9be0873f2aa4d27ec375f68cdcc
hanshal101 <82961842+hanshal101@users.noreply.github.com>
2023-01-21 20:49:42 +0530

    Create SUBSCRIBE.cpp

---
+++ SUBSCRIBE.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    
+	    if(x > 30){
+	        printf("YES\n");
+	    }
+	    else{
+	        printf("NO\n");
+	    }
+	}
+	return 0;
+}
