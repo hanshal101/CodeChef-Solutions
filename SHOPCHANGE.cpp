843e88618f5b5695a543383c8daa1a0ced6ff974
hanshal101 <82961842+hanshal101@users.noreply.github.com>
2023-01-23 16:38:10 +0530

    Create SHOPCHANGE.cpp

---
+++ SHOPCHANGE.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    int sum = 100 - x;
+	    cout<<sum<<endl;
+	}
+	return 0;
+}
