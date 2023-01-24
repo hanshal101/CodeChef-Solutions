2a37e8f2522bf3a500c60c8564718cd9dd7f3724
hanshal101 <82961842+hanshal101@users.noreply.github.com>
2023-01-24 09:37:41 +0530

    Create CREDCOINS.cpp

---
+++ CREDCOINS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    int mult = x * y;
+	    cout<<mult/100<<endl;
+	}
+	return 0;
+}
