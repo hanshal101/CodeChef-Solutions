83e2a4d895528e7149ffd46a2e4bf5267194f1fd
hanshal101 <82961842+hanshal101@users.noreply.github.com>
2023-02-10 15:10:16 +0530

    Create DIFFSUM.cpp

---
+++ DIFFSUM.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n1,n2;
+	cin>>n1>>n2;
+	
+	if(n1>n2){
+	    cout<<n1-n2<<endl;
+	}
+	else {
+	    cout<<n1+n2<<endl;
+	}
+	return 0;
+}
